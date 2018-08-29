df30a6a6aaa941b69b990a1bf5825523baa9c59c
Damion Hardy <damionhardy@hotmail.com>
2018-08-29 15:08:20 +0100

    committing final test code and passing program

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,13 @@ describe 'fizzbuzz2' do
   it 'returns "fizz" for the number 3' do
     expect(fizzbuzz2(3)).to eq 'fizz'
   end
+  it 'returns "buzz" for the number 5' do
+    expect(fizzbuzz2(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" for the number 15' do
+    expect(fizzbuzz2(15)).to eq 'fizzbuzz'
+  end
+  it 'returns the number if none of above satisfied' do
+    expect(fizzbuzz2(1)).to eq 1
+  end
 end
